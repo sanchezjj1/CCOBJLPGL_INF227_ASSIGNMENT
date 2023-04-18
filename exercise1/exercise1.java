420c563ed2ee37543bcaaca4174e189d62e0d066
Justine Jade Sanchez <130418551+sanchezjj1@users.noreply.github.com>
2023-04-18 11:47:50 +0800

    Create exercise1.java

---
+++ exercise1/exercise1.java
@@ -0,0 +1,25 @@
+public class Study { 
+    static boolean iwillReview = true;
+    static boolean iwillNotFail = true;
+
+    public static void main (String[]args) throws Exception {
+	
+        Study myReview = new Study();
+     
+        // precondition
+	assert iwillReview == true: "Panis Pasado!";  
+   
+    myReview.iwillReview();
+    System.out.println(iwillReview);
+   
+        // postcondition
+    assert iwillNotFail == true: "Patay Bagsak Naman!";
+
+    }
+
+    void iwillReview() {
+        iwillReview = false;
+    }
+
+    
+}
